4f49e5fa28d4d103844d4b39612800f64b4d86fe
zhangxinalei <xianlei6868@gmail.com>
2017-08-17 20:06:31 +0800

    set the dll.config.babel

--- webpack.dll.config.babel.js
+++ webpack.dll.config.babel.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
 import HappyPack from 'happypack';
-
+import config from './config'
 const vendors = [
     'jquery',
     'es5-shim',
@@ -18,7 +18,30 @@ module.exports = {
     },
     module: {
         rules: [
-            {}
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'happypack/loader'
+            }
         ]
-    }
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            name: '[name]_dll_[chunkhash]_library',
+            context: __dirname,
+            path: path.join(__dirname, './dist', '[name]_manifest.json'),
+        }),
+        new webpack.HashedModulesIdsPlugin(),
+        new HappyPack({
+            cache: true,
+            loaders: [{
+                loader: 'babel-loader',
+                include: [
+                    path.resolve(__dirname, 'pages'),
+                    path.resolve(__dirname, 'src')
+                ],
+                query: config.babelLoaderQuery
+            }]
+        })
+    ]
 };
